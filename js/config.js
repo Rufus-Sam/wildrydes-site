f0540d7a7e0bf41b7a76f876fb5413e1ef958bb6
Rufus Sam John Immanuel J <68532159+Rufus-Sam@users.noreply.github.com>
2025-12-12 14:34:04 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,10 +2,12 @@ window._config = {
     cognito: {
         userPoolId: 'eu-north-1_qtJ2F23GQ', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '63bmr9onoeapark910g9n1mfus', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientSecret: '1ca7bsuqn6p6faru581cd1j12e0ls79n3fjs3hnd6gdcetvjnfhc', // client secret 
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
